d5ad341044682ced4a226fc8f2bf3895acbabf7d
paulyi326 <paul.yi326@gmail.com>
2014-06-05 21:23:38 -0700

    Uncommented optional tests for binary search tree and graph. All tests passing for binary search tree.

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,4 +1,62 @@
 var makeBinarySearchTree = function(value){
+  var bst = {};
+
+  bst.value = value;
+
+  bst.left = undefined;
+  bst.right = undefined;
+  bst.contains = function(item) {
+    if (bst.value === item) {
+      return true;
+    } else {
+      if (item > bst.value) {
+        if (bst.right === undefined) {
+          return false;
+        } else {
+          return bst.right.contains(item);
+        }
+      } else {
+        if (bst.left === undefined) {
+          return false;
+        } else {
+          return bst.left.contains(item);
+        }
+      }
+    }
+  };
+  bst.insert = function(item) {
+    if (item > bst.value) {
+      //add to the right branch
+      if (bst.right === undefined) {
+        var subtree = makeBinarySearchTree(item);
+        bst.right = subtree;
+      } else {
+        bst.right.insert(item);
+      }
+    } else {
+      //add to the left branch
+      if (bst.left === undefined) {
+        var subtree = makeBinarySearchTree(item);
+        bst.left = subtree;
+      } else {
+        bst.left.insert(item);
+      }
+    }
+  };
+  bst.depthFirstLog = function(iterator) {
+    var recurseDfl = function(tree) {
+      iterator(tree.value);
+      if (tree.left !== undefined) {
+        recurseDfl(tree.left);
+      }
+      if (tree.right !== undefined) {
+        recurseDfl(tree.right);
+      }
+    };
+    recurseDfl(bst);
+  };
+
+  return bst;
 };
 
 
